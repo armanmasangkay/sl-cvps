5954ba5b56a20c2c82fbfebf1f7c10efeba250cd
armanmasangkay <vss.arman@gmail.com>
2021-03-12 13:36:20 +0800

    add registration test

---
+++ tests/Feature/RegistrationTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RegistrationTest extends TestCase
+{
+    public function test_registration_form_can_be_rendered()
+    {
+        $response=$this->get(route('person.registration'));
+        $response->assertViewIs('pages.register');
+        
+    }
+}
