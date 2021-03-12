04fef21bdf725dfef2da682915d4c9308b1d9cc5
Benigno E. Ambus Jr <nobeginmasob@gmail.com>
2021-03-12 14:46:40 +0800

    view registration form

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RegistrationController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/register', [RegistrationController::class, 'view'])->name('person.registration');
 
 Route::get('/reports',function(){
 
