86892365ba8a0b367813fd0c205449821f594eda
armanmasangkay <vss.arman@gmail.com>
2021-03-12 15:44:49 +0800

    add layouts on view

--- tests/Feature/ViewReportsTest.php
+++ tests/Feature/ViewReportsTest.php
@@ -2,8 +2,6 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ViewReportsTest extends TestCase
@@ -14,7 +12,10 @@ class ViewReportsTest extends TestCase
         $response->assertViewIs('pages.superadmin.reports');
     }
 
-    
+
+
+
+
 
 
 }
