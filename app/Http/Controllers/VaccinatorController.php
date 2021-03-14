24016fdf9035b2d50c129c3153f8f8149112bb06
armanmasangkay <vss.arman@gmail.com>
2021-03-14 10:03:33 +0800

    refactor vaccinator  controller and added more tests

--- app/Http/Controllers/VaccinatorController.php
+++ app/Http/Controllers/VaccinatorController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Vaccinator;
 use App\Repositories\Contracts\VaccinatorRepositoryInterface;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class VaccinatorController extends Controller
 {
     private $vaccinatorRepository;
+
     public function __construct(VaccinatorRepositoryInterface $vaccinatorRepository)
     {  
        $this->vaccinatorRepository=$vaccinatorRepository; 
@@ -17,9 +18,29 @@ class VaccinatorController extends Controller
     {
         return view('pages.admin.vaccinator-registration');
     }
+
+    private function createVaccinatorValidator($data)
+    {
+       return Validator::make($data,[
+            'firstname'=>'required',
+            'lastname'=>'required',
+            'position'=>'required',
+            'role'=>'required',
+            'facility'=>'required',
+            'prc'=>'required'
+        ]);
+    }
+
     public function store(Request $request)
     {
+        $validator=$this->createVaccinatorValidator($request->all());
+
+        if($validator->fails()){
+            return redirect(route('vaccinator.create'))->withErrors($validator);
+        }
+        
         $this->vaccinatorRepository->store($request->all());
+
         return redirect(route('vaccinator.create'))->with([
             'created'=>true,
             'message'=>'Vaccinator successfully registered.'
