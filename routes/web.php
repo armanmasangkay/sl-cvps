b8e96b2586514b425b41b5594b98b323b17aeeb1
armanmasangkay <vss.arman@gmail.com>
2021-03-15 16:58:24 +0800

    add redirect to root route to login

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/',function(){
+    return redirect(route('user.login'));
+});
+
 Route::middleware(['auth'])->group(function () {
     Route::resource('admin', AdminController::class);
     Route::resource('facility', FacilityController::class);
