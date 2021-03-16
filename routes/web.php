b5a6e57ed6cd3422821799e7ab043868dfad8dfc
miyunecadz <jvcadz@gmail.com>
2021-03-16 13:45:46 +0800

    web.php

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,6 @@ use App\Http\Controllers\EncoderController;
 use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\PostVaxController;
 use App\Http\Controllers\SuperAdminController;
-use App\Http\Controllers\PreRegisteredController;
 use App\Http\Controllers\UserLogin;
 use App\Models\Vaccination;
 use Illuminate\Support\Facades\Route;
@@ -40,7 +39,6 @@ Route::middleware(['auth'])->group(function () {
     Route::resource('vaccine', VaccineController::class);
     Route::resource('vaccinator',VaccinatorController::class);
     Route::resource('administrator', SuperAdminController::class);
-    Route::resource('pre', PreRegisteredController::class);
     Route::get('/reports/superadmin', [ReportsController::class, 'index'])->name('reports.superadmin');
     Route::get('/reports/admin',[AdminReportsController::class,'index'])->name('reports.admin');
     Route::get('/post-vax',[PostVaxController::class,'index'])->name('encoder.post-vax');
