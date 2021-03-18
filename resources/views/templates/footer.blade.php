1a7b04396c77861c2893d6e344e566b7abf96ff1
Benigno E. Ambus Jr <nobeginmasob@gmail.com>
2021-03-18 12:20:42 +0700

    fix css for registration

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -8,18 +8,18 @@
         @if(isset($_GET['view-from']) && $_GET['view-from'] == 'login')
         <a href="{{ route('user.login') }}" class="mr-2">Home</a>
         @elseif(isset($_GET['view-from']) && $_GET['view-from'] == 'register')
-        <a href="{{ route('developer') }}" class="mr-2">Home</a>
+        <a href="{{ route('developer.info') }}" class="mr-2">Home</a>
         @else
         <a href="{{ route('check-route.index') }}" class="mr-2">Home</a>
         @endif
     @endif
     <a href="" class="mr-2 {{ (Request::is('developer') ? 'ml-2' : '') }}">Privacy Policy</a><a href="" class="mr-2 ml-2">Terms and Condition</a>
     @if(Request::is('login'))
-    <a href="{{ route('developer', ['view-from' => 'login']) }}" class="mr-2 ml-2">Developer</a>
-    @elseif(Request::is('developer'))
-    <a href="{{ route('developer', ['view-from' => 'register']) }}" class="mr-2 ml-2">Developer</a>
+    <a href="{{ route('developer.info', ['view-from' => 'login']) }}" class="mr-2 ml-2">Developer</a>
+    @elseif(Request::is('register'))
+    <a href="{{ route('developer.info', ['view-from' => 'register']) }}" class="mr-2 ml-2">Developer</a>
     @else
-    <a href="{{ route('developer') }}" class="mr-2 ml-2">Developer</a>
+    <a href="{{ route('developer.info') }}" class="mr-2 ml-2">Developer</a>
     @endif
     <a href="" class="mr-2 ml-2">About</a>
     <a href="" class="ml-2">Help</a> </p>
