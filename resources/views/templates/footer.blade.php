5abee88ab7cbf53944c76db26f96b635d498bb6b
Benigno E. Ambus Jr <nobeginmasob@gmail.com>
2021-03-18 10:52:53 +0700

    footer route changes and developer card info changes

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -9,10 +9,18 @@
         <a href="{{ route('user.login') }}" class="mr-2">Home</a>
         @elseif(isset($_GET['view-from']) && $_GET['view-from'] == 'register')
         <a href="{{ route('developer') }}" class="mr-2">Home</a>
+        @else
+        <a href="{{ route('check-route') }}" class="mr-2">Home</a>
         @endif
     @endif
     <a href="" class="mr-2 {{ (Request::is('developer') ? 'ml-2' : '') }}">Privacy Policy</a><a href="" class="mr-2 ml-2">Terms and Condition</a>
-    <a href="{{ route('developer', ['view-from' => (Request::is('login') ? 'login' : 'registration')]) }}" class="mr-2 ml-2">Developer</a>
+    @if((Request::is('login'))
+    <a href="{{ route('developer', ['view-from' => 'login']) }}" class="mr-2 ml-2">Developer</a>
+    @elseif((Request::is('developer'))
+    <a href="{{ route('developer', ['view-from' => 'register']) }}" class="mr-2 ml-2">Developer</a>
+    @else
+    <a href="{{ route('developer') }}" class="mr-2 ml-2">Developer</a>
+    @endif
     <a href="" class="mr-2 ml-2">About</a>
     <a href="" class="ml-2">Help</a> </p>
 </div>
