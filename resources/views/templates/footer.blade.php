9dd5fb387fb7aa69eed0e52b4fa31cbe6775c8f5
Benigno E. Ambus Jr <nobeginmasob@gmail.com>
2021-03-18 10:24:51 +0700

    footer links changes

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -5,9 +5,14 @@
 <div class="container mt-5 pb-3">
     <p class="text-center text-gray text-muted text-footer"> 
     @if(Request::is('developer')) 
-    <a href="{{ route('check-route.index') }}" class="mr-2">Home</a>
+        @if(isset($_GET['view-from']) && $_GET['view-from'] == 'login')
+        <a href="{{ route('user.login') }}" class="mr-2">Home</a>
+        @elseif(isset($_GET['view-from']) && $_GET['view-from'] == 'register')
+        <a href="{{ route('developer') }}" class="mr-2">Home</a>
+        @endif
     @endif
     <a href="" class="mr-2 {{ (Request::is('developer') ? 'ml-2' : '') }}">Privacy Policy</a><a href="" class="mr-2 ml-2">Terms and Condition</a>
-    <a href="{{ route('developer.index') }}" class="mr-2 ml-2">Developer</a><a href="" class="mr-2 ml-2">About</a>
+    <a href="{{ route('developer', ['view-from' => (Request::is('login') ? 'login' : 'registration')]) }}" class="mr-2 ml-2">Developer</a>
+    <a href="" class="mr-2 ml-2">About</a>
     <a href="" class="ml-2">Help</a> </p>
 </div>
