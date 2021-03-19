2890e45f2360bee2623aced931e6407a3542682c
Benigno E. Ambus Jr <nobeginmasob@gmail.com>
2021-03-19 16:06:16 +0700

    custom page error

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Error 400 | Page not found</title>
+    <title>Error 500 | Bad Connection</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="{{ asset('css/custom.css?v1.0') }}">
 </head>
@@ -15,7 +15,7 @@
                 <div class="col-md-8 offset-md-2 col-sm-8 offset-sm-2 image">
                     <div class="">
                         <center>
-                            <img src="{{ asset('images/error/404.jpg') }}" alt="Error 404">
+                            <img src="{{ asset('images/error/500.jpg') }}" alt="Error 500">
                         </center>
                     </div>
                 </div>
